64f54b1649ed114009bd020204ecdd38b6d24e74
aNdr3W03 <andrewbjamesie@yahoo.com>
2023-07-11 00:45:22 +0700

    Update styling

--- src/scripts/utils/websocket-initiator.js
+++ src/scripts/utils/websocket-initiator.js
@@ -1,11 +1,21 @@
+// import NotificationHelper from './notification-helper';
+// import CONFIG from '../globals/config';
+
 const WebSocketInitiator = {
   init(url) {
     const webSocket = new WebSocket(url);
     webSocket.onmessage = this._onMessageHandler;
   },
 
-  _onMessageHandler(message) {
-    console.log(JSON.parse(message.data));
-  },
+  // _onMessageHandler(message) {
+  //   const restaurants = JSON.parse(message.data);
+  //   NotificationHelper.sendNotification({
+  //     title: restaurants.name,
+  //     options: {
+  //       body: restaurants.review,
+  //       image: `${CONFIG.BASE_IMAGE_URL + movie.poster_path}`,
+  //     },
+  //   });
+  // },
 };
 export default WebSocketInitiator;
